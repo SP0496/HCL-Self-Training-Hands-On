e30b0adb75057f722ea146489cff77d28f42372a
SP0496 <satishanadasu8@gmail.com>
2026-04-16 13:18:18 +0530

    SpiralMatrix

---
+++ SpiralMatrix.java
@@ -0,0 +1,53 @@
+public class SpiralMatrix {
+
+    public static void printSpiral(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int top = 0, bottom = m - 1;
+        int left = 0, right = n - 1;
+
+        System.out.print("Spiral Order: ");
+
+        while (top <= bottom && left <= right) {
+
+            // 1. Left → Right
+            for (int i = left; i <= right; i++) {
+                System.out.print(matrix[top][i] + " ");
+            }
+            top++;
+
+            // 2. Top → Bottom
+            for (int i = top; i <= bottom; i++) {
+                System.out.print(matrix[i][right] + " ");
+            }
+            right--;
+
+            // 3. Right → Left
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    System.out.print(matrix[bottom][i] + " ");
+                }
+                bottom--;
+            }
+
+            // 4. Bottom → Top
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    System.out.print(matrix[i][left] + " ");
+                }
+                left++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9,10,11,12}
+        };
+
+        printSpiral(matrix);
+    }
+}
